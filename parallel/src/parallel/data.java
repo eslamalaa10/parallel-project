61bc67bfe8f048399c2bf5d36c2831799ecf5c60
ToniMagdy <toni.magdy261@gmail.com>
2020-12-18 02:47:15 +0200

    parallel

---
+++ parallel/src/parallel/data.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parallel;
+
+/**
+ *
+ * @author Eslam Alaa
+ */
+public class data {
+    public String title;
+    public String content;
+}
